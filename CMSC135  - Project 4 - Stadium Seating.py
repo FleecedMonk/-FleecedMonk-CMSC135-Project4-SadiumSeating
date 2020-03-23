8c6b5729b25d22412766a7e11c72bd4c7b640677
FleecedMonk <36286296+FleecedMonk@users.noreply.github.com>
2020-03-23 14:01:21 -0400

    Getting Started

---
+++ CMSC135  - Project 4 - Stadium Seating.py
@@ -0,0 +1,3 @@
+# Purpose: To make a program that will utilize the "function" of python.
+# This prorgam will ask how many tickets for each class of seats sold, then
+# displays the amount of income generated from ticket sales.
